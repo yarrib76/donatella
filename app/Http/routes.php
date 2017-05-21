68b02e79ca318b4f3db9a3c6ee95d2f651da194f
Yamil <yarrib76@gmail.com>
2017-05-20 23:54:52 -0300

    Primer Push

---
+++ app/Http/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'api'],
+    function () {
+        Route::get('/listar', 'Api\FacturacionH@listar');
+        Route::get('/borrargasto', 'Api\GastosController@delete');
+        Route::get('/consultagastos', 'Api\ReporteController@query');
+        Route::get('/consultagastosanual', 'Api\ReporteController@queryMenAnual');
+        Route::get('/listaitems', 'Api\ListaItemsController@query');
+        Route::get('/cajamin', 'Api\FacturacionH@cajaMin');
+
+    });

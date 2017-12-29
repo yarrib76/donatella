5654cbaf84d237d7905d431ad226aad54496a2e5
yarrib76 <yarrib76@gmail.com>
2017-12-29 16:16:56 +0000

    .

--- app/Http/Controllers/Api/ReportesDashboard.php
+++ app/Http/Controllers/Api/ReportesDashboard.php
@@ -8,16 +8,17 @@ use Illuminate\Http\Request;
 use Donatella\Http\Requests;
 use Donatella\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 class ReportesDashboard extends Controller
 {
 
     public function ventas()
     {
-     //   $año = Input::get('anio');
-     //   if (empty($año)){
+        $año = Input::get('anio');
+        if (empty($año)){
             $año = Carbon::createFromFormat('Y-m-d H:i:s', date("Y-m-d H:i:s"))->year;
-     //   }
+        }
         DB::statement("SET lc_time_names = 'es_ES'");
         $query = DB::select('SELECT DATE_FORMAT(fecha, "%M") AS Mes, ROUND(SUM(CASE WHEN Descuento <> "null" OR Descuento = 0 THEN Descuento ELSE total END),2) as total
                             from samira.facturah where fecha >= "' . $año .'/01/01" and Fecha <= "' . $año .'/12/31" group by Mes');

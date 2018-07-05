62f9c289d1fded4bc21e4c8c3729f045d8cfd8bf
yarrib76 <yarrib76@gmail.com>
2018-07-05 23:23:55 +0000

    Agrego posibilidad de agregar o quitar columnas del abm/clientes clientes

--- resources/views/clientes/reporte.blade.php
+++ resources/views/clientes/reporte.blade.php
@@ -2,40 +2,53 @@
 @section('contenido')
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 ">
+            <div class="col-sm-15 ">
                 <div class="panel panel-primary">
                     <div class="panel-heading"><i class="fa fa-cog">Clientes</i></div>
                     <div class="panel-body">
-                            <table id="reporte" class="table table-striped table-bordered records_list">
-                                <thead>
+                        <div>
+                            Seleccionar Columnas: <a class="toggle-vis" data-column="0">Nombre</a> -
+                            <a class="toggle-vis" data-column="1">Apellido</a> -
+                            <a class="toggle-vis" data-column="2">Apodo</a> -
+                            <a class="toggle-vis" data-column="3">Cuit</a> -
+                            <a class="toggle-vis" data-column="4">Direccion</a> -
+                            <a class="toggle-vis" data-column="5">Mail</a> -
+                            <a class="toggle-vis" data-column="6">Telefono</a> -
+                            <a class="toggle-vis" data-column="7">Localidad</a> -
+                            <a class="toggle-vis" data-column="8">Provincia</a>
+                        </div>
+                        <table id="reporte" class="table table-striped table-bordered records_list">
+                            <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Apodo</th>
+                                <th>Cuit</th>
+                                <th>Direccion</th>
+                                <th>Mail</th>
+                                <th>Telefono</th>
+                                <th>Localidad</th>
+                                <th>Provincia</th>
+                                <th>Accion</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($clientes as $cliente)
                                 <tr>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Apodo</th>
-                                    <th>Cuit</th>
-                                    <th>Direccion</th>
-                                    <th>Mail</th>
-                                    <th>Telefono</th>
-                                    <th>Localidad</th>
-                                    <th>Accion</th>
+                                    <td>{{$cliente->nombre}}</td>
+                                    <td>{{$cliente->apellido}}</td>
+                                    <td>{{$cliente->apodo}}</td>
+                                    <td>{{$cliente->cuit}}</td>
+                                    <td>{{$cliente->direccion}}</td>
+                                    <td>{{$cliente->mail}}</td>
+                                    <td>{{$cliente->telefono}}</td>
+                                    <td>{{$cliente->localidad}}</td>
+                                    <td>{{$cliente->provincia}}</td>
+                                    <td><a href='{{ route('clientes.edit', $cliente->id_clientes) }}' class = 'btn btn-primary'>Modificar</a></td>
                                 </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($clientes as $cliente)
-                                    <tr>
-                                        <td>{{$cliente->nombre}}</td>
-                                        <td>{{$cliente->apellido}}</td>
-                                        <td>{{$cliente->apodo}}</td>
-                                        <td>{{$cliente->cuit}}</td>
-                                        <td>{{$cliente->direccion}}</td>
-                                        <td>{{$cliente->mail}}</td>
-                                        <td>{{$cliente->telefono}}</td>
-                                        <td>{{$cliente->localidad}}</td>
-                                        <td><a href='{{ route('clientes.edit', $cliente->id_clientes) }}' class = 'btn btn-primary'>Modificar</a></td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                            @endforeach
+                            </tbody>
+                        </table>
                         <a href='{{ route('clientes.create') }}' class = 'btn btn-primary'>Crear Cliente</a>
                     </div>
                 </div>
@@ -56,14 +69,45 @@
 
     <script type="text/javascript">
         $(document).ready( function () {
-            $('#reporte').DataTable({
+            var table =  $('#reporte').DataTable({
                         dom: 'Bfrtip',
                         buttons: [
                             'excel'
+                        ],
+                        "columnDefs": [
+                            {
+                                "targets": [ 2 ],
+                                "visible": false,
+                                "searchable": false
+                            },
+                            {
+                                "targets": [ 3 ],
+                                "visible": false,
+                                "searchable": false
+                            },
+                            {
+                                "targets": [ 7 ],
+                                "visible": false,
+                                "searchable": false
+                            },
+                            {
+                                "targets": [ 8 ],
+                                "visible": false,
+                                "searchable": false
+                            }
                         ]
                     }
 
             );
+            $('a.toggle-vis').on( 'click', function (e) {
+                e.preventDefault();
+
+                // Get the column API object
+                var column = table.column( $(this).attr('data-column') );
+
+                // Toggle the visibility
+                column.visible( ! column.visible() );
+            } );
         } );
     </script>
 @stop

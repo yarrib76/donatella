58f5b2068c4d6360b27b09b7b79410ccd4fbf9b6
yarrib76 <yarrib76@gmail.com>
2019-04-06 20:18:57 +0000

    Articulos Tienda Nube

--- app/Http/Controllers/Api/ABMTiendaNube.php
+++ app/Http/Controllers/Api/ABMTiendaNube.php
@@ -15,21 +15,26 @@ class ABMTiendaNube extends Controller
 {
     public function abmProductos()
     {
+        $cantidadPorPaginas = 5;
         //Datos para la conexiòn
         $access_token = 'ce4bf7da2c19529c4f3134ec3cfa20b8a8faf90b';
         $store_id = '972788';
 
         $api = new API($store_id, $access_token, 'Test (yarrib76@gmail.com)');
-        $articulosTiendaNube = $api->get("products");
-        $precioAydua = new Precio();
-        foreach ($articulosTiendaNube->body as $articulo){
-            foreach ($articulo->variants as $variant){
-                $articuloLocal = Articulos::where('Articulo',$variant->sku)->get();
-                if (!$articuloLocal->isEmpty()){
-                    $response = $api->put("products/$variant->product_id/variants/$variant->id", [
-                        'price' => $precioAydua->query($articuloLocal[0])[0]['PrecioVenta'],
-                        'stock' => $articuloLocal[0]['Cantidad']
-                    ]);
+        $cantidadConsultas = $this->obtengoCantConsultas($api,$cantidadPorPaginas);
+
+        for ($i = 1; $i <= $cantidadConsultas; $i++){
+            $articulosTiendaNube = $api->get("products?page=$i&per_page=$cantidadPorPaginas");
+            $precioAydua = new Precio();
+            foreach ($articulosTiendaNube->body as $articulo){
+                foreach ($articulo->variants as $variant){
+                    $articuloLocal = Articulos::where('Articulo',$variant->sku)->get();
+                    if (!$articuloLocal->isEmpty()){
+                        $response = $api->put("products/$variant->product_id/variants/$variant->id", [
+                            'price' => $precioAydua->query($articuloLocal[0])[0]['PrecioVenta'],
+                            'stock' => $this->verificoStock($articuloLocal[0])
+                        ]);
+                    }
                 }
             }
         }
@@ -38,8 +43,15 @@ class ABMTiendaNube extends Controller
     public function verificoStock($articulo)
     {
         if ($articulo->Cantidad >= 4) {
-            return "InStock";
+            return $articulo->Cantidad;
         }
-        return "OutOfStock";
+        return "";
+    }
+
+    private function obtengoCantConsultas($api,$cantidadPorPaginas)
+    {
+        $query = $api->get("products?page=1&per_page=1");
+        $cantidadConsultas = (ceil(($query->headers['x-total-count'] / $cantidadPorPaginas)));
+        return $cantidadConsultas;
     }
 }

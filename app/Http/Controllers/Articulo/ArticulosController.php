3467f784f0fd866830b4d01d127553d91edb47cd
yarrib76 <yarrib76@gmail.com>
2019-06-17 20:37:19 +0000

    Agregue la cantidad de articulos que hay en pedidos en Aritculos-ABM

--- app/Http/Controllers/Articulo/ArticulosController.php
+++ app/Http/Controllers/Articulo/ArticulosController.php
@@ -36,11 +36,11 @@ class ArticulosController extends Controller
     public function index()
     {
         $articulos = DB::Select ('SELECT Arti.Articulo as Articulo, Arti.Detalle as Detalle, Arti.Cantidad as Cantidad,
-                                    count(pedidotemp.Cantidad) as Pedido, repoArt.PrecioVenta as PrecioVenta, Arti.ImageName,
-                                    Arti.Web
+                                    sum(if(Control.estado = 1, pedidotemp.Cantidad,0)) as Pedido, repoArt.PrecioVenta as PrecioVenta, Arti.ImageName, Arti.Web
                                     FROM samira.articulos as Arti
                                     left join samira.pedidotemp as pedidoTemp On Arti.Articulo = pedidoTemp.Articulo
                                     inner join samira.reportearticulo as repoArt On Arti.Articulo = repoArt.Articulo
+                                    left join samira.controlpedidos as Control ON pedidotemp.NroPedido = Control.nropedido
                                     group by Arti.Articulo');
         //dd($articulos);
         /** Se cambia para incorpoar Cantidad en PedidosTemp

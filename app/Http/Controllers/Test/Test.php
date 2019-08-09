071b61bec8e2b05e267aa85f63dab5bebdc20eb9
yarrib76 <yarrib76@gmail.com>
2019-08-09 15:54:51 +0000

    Error ReporteArticuliProveedor

--- app/Http/Controllers/Test/Test.php
+++ app/Http/Controllers/Test/Test.php
@@ -23,6 +23,7 @@ class Test extends Controller
         $articulosPreoveedores[] = [];
         DB::select('truncate table samira.reportearticulo');
         foreach ($articulos as $articulo) {
+            printf("Articulo: " + $articulo->Articulo);
             if (!is_null($articulo->Proveedor)) {
                 $precio = $precioAydua->query($articulo);
                 $proveedor = Proveedores::where('Nombre', $articulo->Proveedor)->get();

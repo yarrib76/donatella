3e1134b1803fca21dea52cb01c8c21c143dea767
yarrib76 <yarrib76@gmail.com>
2019-08-15 16:22:37 +0000

    Comentarios Pedidos

--- app/Http/Controllers/Pedido/PedidosController.php
+++ app/Http/Controllers/Pedido/PedidosController.php
@@ -25,16 +25,30 @@ class PedidosController extends Controller
      */
     public function index()
     {
+        $user_id = Auth::user()->id;
+        $role = Auth::user()->id_roles;
         DB::statement("SET lc_time_names = 'es_ES'");
-        $pedidos = DB::select('SELECT DATE_FORMAT(pedidos.fecha, "%d de %M %Y") AS fecha, pedidos.fecha as fechaParaOrden, nroPedido as nropedido, clientes.nombre as nombre,
+        /* Si el rol es de vendedora solo miestro los pedidos en proceso, es para agilizar la vista en la tablet*/
+        if ($role == 3){
+            $pedidos = DB::select('SELECT DATE_FORMAT(pedidos.fecha, "%d de %M %Y") AS fecha, pedidos.fecha as fechaParaOrden, nroPedido as nropedido, clientes.nombre as nombre,
+                    clientes.apellido as apellido, pedidos.nrofactura, pedidos.vendedora, pedidos.estado, pedidos.id as id, pedidos.total as total,
+                    pedidos.ordenweb as ordenweb, comentarios.comentario as comentarios
+                    from samira.controlPedidos as pedidos
+                    INNER JOIN samira.clientes as clientes ON clientes.id_clientes = pedidos.id_cliente
+                    left join samira.comentariospedidos as comentarios ON comentarios.controlpedidos_id = pedidos.id
+                    where pedidos.estado = 1 or pedidos.estado = 2
+                    group by nropedido');
+        } else {
+            $pedidos = DB::select('SELECT DATE_FORMAT(pedidos.fecha, "%d de %M %Y") AS fecha, pedidos.fecha as fechaParaOrden, nroPedido as nropedido, clientes.nombre as nombre,
                     clientes.apellido as apellido, pedidos.nrofactura, pedidos.vendedora, pedidos.estado, pedidos.id as id, pedidos.total as total,
                     pedidos.ordenweb as ordenweb, comentarios.comentario as comentarios
                     from samira.controlPedidos as pedidos
                     INNER JOIN samira.clientes as clientes ON clientes.id_clientes = pedidos.id_cliente
                     left join samira.comentariospedidos as comentarios ON comentarios.controlpedidos_id = pedidos.id
                     group by nropedido');
-       // dd($pedidos[0]->nroPedido);
-        $user_id = Auth::user()->id;
+            // dd($pedidos[0]->nroPedido);
+        }
+
         return view('pedidos.reporte', compact('pedidos','user_id'));
     }
 

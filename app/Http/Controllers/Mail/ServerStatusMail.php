2f3c0fda841a4e754517fbafc64284f3d15600b9
yarrib76 <yarrib76@gmail.com>
2020-07-11 18:33:26 +0000

    Estado de servicios de repli y backup mysql

--- app/Http/Controllers/Mail/ServerStatusMail.php
+++ app/Http/Controllers/Mail/ServerStatusMail.php
@@ -27,7 +27,7 @@ class ServerStatusMail extends Controller
                 $mysqli = new mysqli("10.10.10.150", "root", "NetAcc10", "samira");
                 break;
             case 'dbweb01':
-                $mysqli = new mysqli("192.168.0.20", "yarrib76", "NetAcc10", "samira");
+                $mysqli = new mysqli("192.168.0.8", "yarrib76", "NetAcc10", "samira");
                 break;
         }
         $result = $mysqli->query("SELECT
